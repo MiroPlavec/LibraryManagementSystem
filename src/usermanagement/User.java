bad6ba3210f82ede1ac17ae926dbd0bb8b1baf7f
Miroslav Plavec <miroslav.plav@gmail.com>
2024-11-27 16:50:54 +0100

    I added some more fields like email or adress

--- src/usermanagement/User.java
+++ src/usermanagement/User.java
@@ -5,14 +5,24 @@ public abstract class User {
     private String firstName;
     private String lastName;
     private String gender;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
 
-    public User(String firstName, String lastName, String gender){
+
+    public User(String firstName, String lastName, String gender, String email, String password, String phoneNumber, String address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
 
-    public User(String firstName, String lastName){
-        this(firstName, lastName, "");
+    public User(String firstName, String lastName, String email, String password){
+        this(firstName, lastName, "", email, password, "", "");
     }
+
 }
