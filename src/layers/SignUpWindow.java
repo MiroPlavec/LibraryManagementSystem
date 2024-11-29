c8eed812c1fa207587a0cccaa03973e568c82d4d
Miroslav Plavec <miroslav.plav@gmail.com>
2024-11-29 13:07:11 +0100

    I create basic skeleton of window where user can sign up.

--- src/layers/SignUpWindow.java
+++ src/layers/SignUpWindow.java
@@ -1,19 +1,60 @@
 package layers;
 
 import helpers.Helper;
+import usermanagement.Customer;
 
 public class SignUpWindow {
 
-    public void collectUserInformation(){
+    public Customer collectUserInformation(){
         System.out.println(" ".repeat(20) + "Sign Up");
         System.out.println("Please enter information about you to complete registration.");
         System.out.println("Option marked with * are required.");
         System.out.println("_ ".repeat(50));
 
-        System.out.print("First name: ");
-        Helper.scanner.nextLine();
-        System.out.print("Last name: ");
-        Helper.scanner.nextLine();
+        String firstName = getUserInfo("*First name: ");
+        String lastName = getUserInfo("*Last name: ");
+        String email = checkEmail();
+        String password = checkPassword();
+
+        System.out.print("Gender: ");
+        String gender = Helper.scanner.nextLine();
+        System.out.print("phoneNumber: ");
+        String phoneNumber = Helper.scanner.nextLine();
+        System.out.print("Address: ");
+        String address = Helper.scanner.nextLine();
+
+        System.out.print("Please confirm your registration with 'yes', any other option will result to cancel your registration.");
+        String input = Helper.scanner.nextLine();
+        if(input.equals("yes")) return new Customer(firstName, lastName, email, password, gender, phoneNumber, address);
+
+        return null;
+    }
+
+    private String getUserInfo(String text){
+        String input = "";
+        while (input.isBlank()) {
+            System.out.print(text);
+            input = Helper.scanner.nextLine();
+        }
+        return input;
+    }
+
+    private String checkEmail(){
+        String input = "";
+        while (input.isBlank() || !input.contains("@")){
+            System.out.print("*Email:");
+            input = Helper.scanner.nextLine();
+        }
+        return input;
+    }
+
+    private String checkPassword(){
+        String input = "";
+        while (input.isBlank()) {
+            System.out.print("*Password: ");
+            input = Helper.scanner.nextLine();
+        }
+        return input;
     }
 
 }
